7aa090b10ec6d13ecb0f58f95bd9fd8c08dd20f0
filipeventuri <filipeventuri@icloud.com>
2025-04-15 19:15:07 -0300

    feat: Criação do model Client

---
+++ models/Client/Client.js
@@ -0,0 +1,58 @@
+const sequelize = require("sequelize");
+const connection = require("../../database/database");
+
+const Client = connection.define("clients", {
+    name: {
+      type: sequelize.STRING,
+      allowNull: false
+    },
+    password:{
+        type: sequelize.INTEGER(6),
+        allowNull:false
+    },
+    email: {
+      type: sequelize.STRING,
+      allowNull: true
+    },
+    phone: {
+      type: sequelize.INTEGER,
+      allowNull: false
+    },
+    document: {
+      type: sequelize.INTEGER,
+      allowNull: false
+    },
+    street: {
+        type: sequelize.TEXT,
+        allowNull: false,
+    },
+    number: {
+        type: sequelize.STRING,
+        allowNull: false,
+    },
+    complement: {
+        type: sequelize.STRING,
+        allowNull: true,
+    },
+    neighborhood: {
+        type: sequelize.STRING,
+        allowNull: false,
+    },
+    city: {
+        type: sequelize.STRING,
+        allowNull: false,
+    },
+    state: {
+        type: sequelize.STRING(2),
+        allowNull: false
+    },
+    zipCode: {
+        type: sequelize.STRING(8),
+        allowNull: false
+    }
+  });
+
+
+Client.sync({force:false});
+
+module.exports = Client;
