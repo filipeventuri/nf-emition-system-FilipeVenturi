4be83204d3e50f055dc2fc9c8ff31143c70e4676
filipeventuri <filipeventuri@icloud.com>
2025-04-15 15:45:11 -0300

    feat: adiciona a tela index

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const connection = require('./database/database');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 connection.authenticate().then(()=>{
     console.log("Database acessed")
 }).catch((err)=>{
     console.log("err: " + err);
 });
+
+app.get('/', (req,res)=>{
+    res.render('index');
+})
 
 app.listen(8080, ()=>{
     console.log('Server Running!')
